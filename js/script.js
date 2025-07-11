f2056ca40a05ac23bde9b5c65b7dd8a826eccc15
BondoDev <146034882+BondoDev@users.noreply.github.com>
2025-07-11 20:52:46 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,49 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+////////////// History popup //////////////
+document.getElementById("historyModal").style.display = "none";
+
+document
+  .querySelector(".section__history button")
+  .addEventListener("click", () => {
+    document.getElementById("historyModal").style.display = "flex";
+  });
+
+document.getElementById("closeHistoryModal").addEventListener("click", () => {
+  document.getElementById("historyModal").style.display = "none";
+});
+
+document.getElementById("historyModal").addEventListener("click", (e) => {
+  if (e.target.id === "historyModal") {
+    e.currentTarget.style.display = "none";
+  }
+});
+
+////////////// Winemaking popup //////////////
+
+document.getElementById("seeHowItMadeBtn").addEventListener("click", () => {
+  document.getElementById("winemakingModal").style.display = "flex";
+});
+
+// Show modal
+document
+  .querySelector(".winemaking .text_and_button_continer button")
+  .addEventListener("click", () => {
+    document.getElementById("winemakingModal").style.display = "flex";
+  });
+
+// Close modal
+document
+  .getElementById("closeWinemakingModal")
+  .addEventListener("click", () => {
+    document.getElementById("winemakingModal").style.display = "none";
+  });
+
+// Close when clicking outside the content
+document.getElementById("winemakingModal").addEventListener("click", (e) => {
+  if (e.target.id === "winemakingModal") {
+    e.currentTarget.style.display = "none";
+  }
+});
